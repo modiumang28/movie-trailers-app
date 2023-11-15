5fab689236e4415ae7b106ad5bf6a01107afaedb
Umang Modi <umangkumarmodi@gmail.com>
2023-11-15 14:21:33 +0530

    feat(app): fetch data from API and set movies state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React, { useState, useEffect } from "react";
+import useGetMovieList from "./customHooks/useGetMovieList/useGetMovieList";
 import AppliedFilters from "./layouts/AppliedFilters/AppliedFilters";
 import Body from "./layouts/Body/Body";
 import Header from "./layouts/Header/Header";
-import "./App.css";
+const url = "https://in.bmscdn.com/m6/static/interview-mock/data.json";
 
 function App() {
+  const [movies, setMovies] = useState(null);
+
+  const { isLoading, isSuccess, isError, data, errorMessage } =
+    useGetMovieList(url);
+
+  // useEffect to set movies state
+  useEffect(() => {
+    if (isSuccess) {
+      setMovies(Object.values(data.moviesData).slice(0, 102));
+    }
+  }, [isSuccess]);
+
   return (
     <div className="App">
       <Header />
       <AppliedFilters />
-      <Body />
+      <Body movies={movies} />
     </div>
   );
 }
