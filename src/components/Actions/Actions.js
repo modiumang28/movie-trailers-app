13f9c4d0e3ff9f9c1772055cfb410cbfe0956751
Umang Modi <umangkumarmodi@gmail.com>
2023-11-15 16:45:34 +0530

    feat(actions): set up logic to fetch and render languages

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+import useGetMovieList from "../../customHooks/useGetMovieList/useGetMovieList";
 import Dropdown from "../common/Dropdown/Dropdown";
+const url = "https://in.bmscdn.com/m6/static/interview-mock/data.json";
 
 const Actions = ({ appliedFilterList, onAppliedFilter }) => {
+  const [languages, setLanguages] = useState([]);
+
+  const { isLoading, isSuccess, isError, data, errorMessage } =
+    useGetMovieList(url);
+
+  useEffect(() => {
+    if (isSuccess && languages.length === 0 && data) {
+      const { languageList } = data;
+      const modifiedLanguageList = languageList.map((lang, index) => {
+        return { value: lang, id: index };
+      });
+      setLanguages([...modifiedLanguageList]);
+    }
+  }, [isSuccess, languages, data]);
+
   return (
     <div className="dropdownAll-container">
       <Dropdown
         name="language"
-        options={[]}
+        options={languages}
         keyToRead={"value"}
         placeholder={"All Languages"}
         onSelect={onAppliedFilter}
