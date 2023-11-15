aeecc7ef7be136e3baf0fba47d08b1f5e1d7e890
Umang Modi <umangkumarmodi@gmail.com>
2023-11-15 14:29:22 +0530

    feat(body): render movies

--- src/layouts/Body/Body.js
+++ src/layouts/Body/Body.js
@@ -1,5 +1,20 @@
-const Body = () => {
-  return <h2>Body</h2>;
+const Body = ({ movies }) => {
+  const itemsPerRow = 6;
+  return (
+    <div className="movie-container">
+      {movies?.map((movie, index) => (
+        <>
+          {index % itemsPerRow === 0 && (
+            <div key={`row-${index / itemsPerRow}`} className="rol">
+              {movies?.slice(index, index + itemsPerRow).map((rowItem) => (
+                <h1>{rowItem.EventTitle}</h1>
+              ))}
+            </div>
+          )}
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default Body;
