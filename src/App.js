8e0a4030df798a994e704cceb52c34291825f3b3
Umang Modi <umangkumarmodi@gmail.com>
2023-11-15 13:46:51 +0530

    feat: add Button, Header and AppliedFilers component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import AppliedFilters from "./layouts/AppliedFilters/AppliedFilters";
+import Body from "./layouts/Body/Body";
+import Header from "./layouts/Header/Header";
 import "./App.css";
 
 function App() {
---
+++ src/layouts/AppliedFilters/AppliedFilters.js
@@ -0,0 +1,5 @@
+const AppliedFilters = () => {
+  return <h2>AppliedFilters</h2>;
+};
+
+export default AppliedFilters;
